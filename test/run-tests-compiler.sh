2870b31a0325032ccdd4d173e977eec9daa3f592
Gabriel Gregori Manzano <gabig307@gmail.com>
2011-10-20 20:44:15 +0200

    Added a test for escaping characters and special inputs.

--- test/run-tests-compiler.sh
+++ test/run-tests-compiler.sh
@@ -13,20 +13,23 @@ echo "============================================"
 echo "* Running tests..."
 echo "* input = $input"
 echo "* expected_output = $output"
-echo "============================================"
+echo ""
 
 for arg in `ls $output` ; do
   inputarg=$input$arg
   inputarg=${inputarg/.v1x/.t1x}
   inputarg=${inputarg/.v2x/.t2x}
   inputarg=${inputarg/.v3x/.t3x}
+  inputarg=${inputarg/.v4x/.t4x}
   ./apertium-transfervm-compiler -i $inputarg -d compiler.log > compiler.out
   if diff compiler.out $output$arg > test_results.log ; then
-    echo $arg "-- OK"
+    echo "+" $arg "-- OK"
   else
-    echo $arg "-- Error"
+    echo "-" $arg "-- Error"
     #cat test_results.log
     fi
 done
+
+echo "============================================"
 
 rm -f compiler.log compiler.out test_results.log
--- test/run-tests-vm.sh
+++ test/run-tests-vm.sh
@@ -2,6 +2,7 @@
 input=test/input/chunker/
 output=test/expected_output/vm/
 code=test/expected_output/compiler
+indoutput=test/expected_output/individual_tests
 
 echo "============================================"
 echo "* Building the vm..."
@@ -14,18 +15,41 @@ echo "* Running tests..."
 echo "* input = $input"
 echo "* expected_output = $output"
 echo "* code = $code"
-echo "============================================"
+echo ""
 
+#Full tests.
 for arg in `ls $output` ; do
   inputarg=$input$arg.txt
   cat $inputarg | ./apertium-transfervm -c $code/apertium-en-ca.en-ca.v1x 2> test_warnings.log | ./apertium-transfervm -c $code/apertium-en-ca.en-ca.v2x 2> test_warnings.log | ./apertium-transfervm -c $code/apertium-en-ca.en-ca.v3x > vm.out 2> test_warnings.log
   if diff vm.out $output$arg > test_results.log ; then
-    echo $arg "-- OK"
+    echo "+" $arg "-- OK"
   else
-    echo $arg "-- Error"
+    echo "-" $arg "-- Error"
     #cat test_results.log
     fi
 done
+
+echo "============================================"
+echo ""
+
+#Individual tests:
+echo "============================================"
+echo "* Running individual tests..."
+echo "* expected_output = $indoutput"
+echo ""
+
+#Test escaping characters inside the chunk content.
+name=escape-sme-nob
+./apertium-transfervm -c $code/apertium-sme-nob.sme-nob.v4x -i test/input/postchunk/$name 2> test_warnings.log > vm.out
+  if diff vm.out $indoutput/$name > test_results.log ; then
+    echo "+" $name "-- OK"
+  else
+    echo "-" $name "-- Error"
+    #cat test_results.log
+    fi
+
+echo "============================================"
+echo ""
 
 rm -f vm.out test_results.log test_warnings.log
 make clean
