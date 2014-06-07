8fda83e177c1a82ab5b2ee4e8de14fd9d49e25cf
Andrei Sfrent <andrei@asfrent.net>
2014-06-07 04:02:45 +0300

    Added tests based on es-en pair.

---
+++ new_test/_testfiles/es-en/config.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function generate_stage1_pretransfer() {
+  local TXTFILE_PATH="${1}"
+  local PRETRANSFER_PATH="${2}"
+  local LANG_DATA="${3}"
+
+  cat "${TXTFILE_PATH}" |\
+    apertium-destxt |\
+    lt-proc "${LANG_DATA}/es-en.automorf.bin" |\
+    apertium-tagger -g "${LANG_DATA}/es-en.prob" |\
+    apertium-pretransfer |\
+    lt-proc -b "${LANG_DATA}/es-en.autobil.bin" > ${PRETRANSFER_PATH}
+}
+
+function generate_stage1_expected() {
+  local PRETRANSFER_PATH="${1}"
+  local RULESXML_PATH="${2}"
+  local RULESXMLBIN_PATH="${3}"
+  local EXPECTED_PATH="${4}"
+
+  cat "${PRETRANSFER_PATH}" |\
+    apertium-transfer -b ${RULESXML_PATH} ${RULESXMLBIN_PATH} > ${EXPECTED_PATH}
+}
+
+function generate_stage1_to_stage2_expected() {
+  local LAST_STAGE_EXPECTED="${1}"
+  local RULESXML_PATH="${2}"
+  local RULESXMLBIN_PATH="${3}"
+  local EXPECTED_PATH="${4}"
+
+  cat "${LAST_STAGE_EXPECTED}" |\
+    apertium-interchunk ${RULESXML_PATH} ${RULESXMLBIN_PATH} > ${EXPECTED_PATH}
+}
+
+function generate_stage2_to_stage3_expected() {
+  local LAST_STAGE_EXPECTED="${1}"
+  local RULESXML_PATH="${2}"
+  local RULESXMLBIN_PATH="${3}"
+  local EXPECTED_PATH="${4}"
+
+  cat "${LAST_STAGE_EXPECTED}" |\
+    apertium-postchunk ${RULESXML_PATH} ${RULESXMLBIN_PATH} > ${EXPECTED_PATH}
+}
